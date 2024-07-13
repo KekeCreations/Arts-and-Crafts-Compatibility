785fba8e5c7bea94d48c28b9de31fe7e7e1b4ae5
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-07-13 02:12:23 +0100

    woops

--- fabric/src/main/java/com/kekecreations/arts_and_crafts_compatibility/compat/caffeinated/CaffeinatedBlocks.java
+++ fabric/src/main/java/com/kekecreations/arts_and_crafts_compatibility/compat/caffeinated/CaffeinatedBlocks.java
@@ -1,9 +1,6 @@
 package com.kekecreations.arts_and_crafts_compatibility.compat.caffeinated;
 
 import com.kekecreations.arts_and_crafts_compatibility.common.compat.CompatUtils;
-import com.kekecreations.arts_and_crafts_compatibility.core.platform.Services;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 
@@ -16,12 +13,8 @@ public class CaffeinatedBlocks {
 
     static {
         for (DyeColor colours : DyeColor.values()) {
-            DYED_COFFEE_SHRUB_FLOWER_POTS.put(colours, registerBlock(colours + "_potted_coffee_shrub", () -> CompatUtils.flowerPot(com.chikoritalover.caffeinated.registry.CaffeinatedBlocks.COFFEE_SHRUB, colours)));
+            DYED_COFFEE_SHRUB_FLOWER_POTS.put(colours, CompatUtils.registerBlock(colours + "_potted_coffee_shrub", () -> CompatUtils.flowerPot(com.chikoritalover.caffeinated.registry.CaffeinatedBlocks.COFFEE_SHRUB, colours)));
         }
-    }
-
-    private static <T extends Block> Supplier<T> registerBlock(String name, Supplier<T> blockSupplier) {
-        return Services.REGISTRY.registerBlock(name, blockSupplier);
     }
 
     public static Block getDyedPottedCoffeeShrub(DyeColor dyeColor) {

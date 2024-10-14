e5a0d07c574089f8cdd8ff0c4e8ec092afbceeb3
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2024-10-14 22:00:35 +0100

    V1.2.2 hotfix

--- common/src/main/java/com/kekecreations/arts_and_crafts_compatibility/core/mixin/PalisadeBlockMixin.java
+++ common/src/main/java/com/kekecreations/arts_and_crafts_compatibility/core/mixin/PalisadeBlockMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PalisadeBlock.class)
 public class PalisadeBlockMixin {
     
-    @Inject(method = "canConnect(Lnet/minecraft/world/level/block/state/BlockState;ZLnet/minecraft/core/Direction;)Z", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    @Inject(method = "canConnect(Lnet/minecraft/world/level/block/state/BlockState;ZLnet/minecraft/core/Direction;)Z", at = @At(value = "HEAD"), cancellable = true)
     public void arts_and_crafts_compat_canConnect(BlockState state, boolean flag0, Direction direction, CallbackInfoReturnable<Boolean> cir) {
         Block block = state.getBlock();
         if (block instanceof ACCPalisadeBlock) {
